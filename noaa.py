22582f2e713d76d01689016f73e6fae75f35cb4e
Stuart Clark <stuart.clark_45@live.co.uk>
2019-10-19 20:18:25 +0100

    noaa: MAX_LIMIT and make data(..) generator

--- noaa.py
+++ noaa.py
@@ -1,7 +1,9 @@
-import requests
 from datetime import datetime
 
+import requests
+
 BASE_URL = "https://www.ncdc.noaa.gov/cdo-web/api/v2/"
+MAX_LIMIT = 1000
 
 
 class NOAA:
@@ -16,21 +18,21 @@ class NOAA:
     def locations_categories(self):
         return requests.get(
             BASE_URL + "locationcategories",
-            params={"limit": 1000},
+            params={"limit": MAX_LIMIT},
             headers=self.default_headers,
         ).json()
 
     def locations(self, location_category_id: str = None):
         return requests.get(
             BASE_URL + "locations",
-            params={"limit": 1000, "locationcategoryid": location_category_id},
+            params={"limit": MAX_LIMIT, "locationcategoryid": location_category_id},
             headers=self.default_headers,
         ).json()
 
     def stations(self, location_id=None):
         return requests.get(
             BASE_URL + "stations",
-            params={"limit": 1000, "locationid": location_id},
+            params={"limit": MAX_LIMIT, "locationid": location_id},
             headers=self.default_headers,
         ).json()
 
@@ -40,14 +42,14 @@ class NOAA:
             url += "/" + data_set_id
         return requests.get(
             url,
-            params={"limit": 1000},
+            params={"limit": MAX_LIMIT},
             headers=self.default_headers,
         ).json()
 
     def data_categories(self):
         return requests.get(
             BASE_URL + "datacategories",
-            params={"limit": 1000},
+            params={"limit": MAX_LIMIT},
             headers=self.default_headers,
         ).json()
 
@@ -57,21 +59,50 @@ class NOAA:
             url += "/" + data_type_id
         return requests.get(
             url,
-            params={"limit": 1000, "datacategoryid": data_category_id},
+            params={"limit": MAX_LIMIT, "datacategoryid": data_category_id},
             headers=self.default_headers,
         ).json()
 
-    def data(self, data_set_id, start_date: datetime, end_date: datetime = None, data_type_id: str = None):
+    def data(self,
+             data_set_id,
+             start_date: datetime,
+             end_date: datetime = None,
+             data_type_id: str = None,
+             pages: int = None):
+
         if not end_date:
             end_date = datetime.now()
 
-        return requests.get(
-            BASE_URL + "data",
-            params={
-                "datasetid": data_set_id,
-                "datatypeid": data_type_id,
-                "startdate": start_date.isoformat(),
-                "enddate": end_date.isoformat(),
-            },
-            headers=self.default_headers,
-        ).json()
+        page_count = 0
+        page_limit = MAX_LIMIT
+        offset = 1
+        count = None
+        while count is None or offset + page_limit < count:
+            response = requests.get(
+                BASE_URL + "data",
+                params={
+                    "limit": page_limit,
+                    "offset": 1,
+                    "datasetid": data_set_id,
+                    "datatypeid": data_type_id,
+                    "startdate": start_date.isoformat(),
+                    "enddate": end_date.isoformat(),
+                },
+                headers=self.default_headers,
+            ).json()
+
+            if "metadata" not in response:
+                raise Exception(f"Request failed {response}")
+
+            results_set = response["metadata"]["resultset"]
+            offset = results_set["offset"]
+            count = results_set["count"]
+            # Just in case the limit is changed by the API
+            page_limit = results_set["limit"]
+
+            for result in response["results"]:
+                yield result
+
+            page_count += 1
+            if pages == page_count:
+                return
