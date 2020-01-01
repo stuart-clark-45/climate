b7f7ce476ba5ad710e86faf5bb01eeafb968bb62
Stuart Clark <stuart.clark_45@live.co.uk>
2020-01-01 17:58:40 +0000

    main: imports, using station interpolator

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from pandas import DataFrame
 from geopandas import GeoDataFrame
 from mongoengine import connect
 
-from geo_point import GeoPoint
+from src.geo_point import GeoPoint
+from src.station_interpolator import StationInterpolator
 from src.geo_util import geo_sample_grid
-from station_data_importer import import_station_data
+from src.station_data_importer import import_station_data
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,53 +20,6 @@ connect('climate')
 
 def print_json(obj):
     print(json.dumps(obj, indent=2))
-
-
-# TODO this is pretty inefficient because of the use of DataFrame.iterrows()
-# TODO https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
-def interpolate_station_data(
-        station_df: DataFrame,
-        samples_df: DataFrame,
-        threshold=250,
-        neighbourhood_size=3,
-        distance_exponent=2):
-
-    sample_values = []
-
-    # For each of the samples we are going to find an interpolated value
-    for _, sample in samples_df.iterrows():
-
-        # Find the samples neighbours
-        neighbourhood = []
-        for _, station in station_df.iterrows():
-
-            # Get the distance from the station to the sample
-            station_point = GeoPoint(station["longitude"], station["latitude"])
-            sample_point = GeoPoint(sample["longitude"], sample["latitude"])
-            distance = station_point.distance_km(sample_point)
-
-            if distance < threshold:
-                neighbourhood.append({"distance": distance, "value": station["value"]})
-
-        # Select at most the three closest neighbours
-        neighbourhood.sort(key=lambda x: x["distance"])
-        neighbourhood = neighbourhood[:neighbourhood_size]
-
-        # Make sure the sample has a neighborhood
-        if not len(neighbourhood):
-            raise Exception("Increase threshold at least one sample has no neighbours")
-
-        # Calculate the inverse distance weighted value
-        sum_distance = sum(map(lambda n: n["value"], neighbourhood))
-        interpolated_value = 0.0
-        for neighbour in neighbourhood:
-            modifier = sum_distance / neighbour["distance"] ** distance_exponent
-            interpolated_value += neighbour["value"] * modifier
-
-        sample_values.append(interpolated_value)
-
-    # Modify the samples data frame
-    samples_df["value"] = sample_values
 
 
 def main():
@@ -105,7 +59,7 @@ def main():
         if len(df_date.station.unique()) != len(df_date):
             raise Exception('Multiple data points for same date and station')
 
-        interpolate_station_data(df_date, samples_df)
+        StationInterpolator().interpolate_station_data(df_date, samples_df)
 
         # station_points = [GeoPoint(row["longitude"], row["latitude"]) for index, row in df_date.iterrows()]
         # gdf = GeoDataFrame(df_date, geometry=station_points)
